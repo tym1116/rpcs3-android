1029781afdfbf90be41561f09153b993fb2bf80f
Alexoqool <154623493+Alexoqool@users.noreply.github.com>
2025-02-27 22:09:01 +0300

    MainActivity.kt: Request storage permission

--- app/src/main/java/net/rpcs3/MainActivity.kt
+++ app/src/main/java/net/rpcs3/MainActivity.kt
@@ -1,18 +1,48 @@
 package net.rpcs3
 
 import android.content.Intent
+import android.net.Uri
 import android.opengl.GLSurfaceView
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
+import androidx.activity.result.contract.ActivityResultContracts
 import net.rpcs3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var openGl: GLSurfaceView
+
+    private val storagePermission = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (Environment.isExternalStorageManager()) {
+            appInit()
+        } else {
+            finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        if (!Environment.isExternalStorageManager()) {
+            requestStoragePermission()
+        } else {
+            initializeApp()
+        }
+    }
+
+    private fun requestStoragePermission() {
+        val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION).apply {
+            data = Uri.parse("package:$packageName")
+        }
+        storagePermission.launch(intent)
+    }
+
+    private fun appInit() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         openGl = glView(this)
         // setContentView(openGl) Set instead of activity or SurfaceView
