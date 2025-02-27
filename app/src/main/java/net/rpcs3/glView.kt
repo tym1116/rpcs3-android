61537a61394b7448993622fa9d7bbce33627bf92
Alexoqool <154623493+Alexoqool@users.noreply.github.com>
2025-02-27 12:25:23 +0300

    Update glView.kt

--- app/src/main/java/net/rpcs3/glView.kt
+++ app/src/main/java/net/rpcs3/glView.kt
@@ -7,7 +7,7 @@ class glView(context: Context) : GLSurfaceView(context) {
     private val renderer: glRenderer
     init {
         // Создаем контекст для 3 версии
-        setEGLContextClientVersion(2)
+        setEGLContextClientVersion(3)
 
         renderer = glRenderer()
 
