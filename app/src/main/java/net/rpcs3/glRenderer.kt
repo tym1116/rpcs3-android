d76d79ab159e3da563dbccc5ed657aa97bffa4ea
MtkBoy <143905189+mtkboy1@users.noreply.github.com>
2025-02-27 11:17:46 +0300

    Fix crash

--- app/src/main/java/net/rpcs3/glRenderer.kt
+++ app/src/main/java/net/rpcs3/glRenderer.kt
@@ -9,11 +9,11 @@ class glRenderer : GLSurfaceView.Renderer {
         GLES30.glClearColor(0.0f,0.0f,0.0f,1.0f);
     }
 
-    override fun onSurfaceChanged(p0: GL10?, p1: Int, p2: Int) {
-        GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
+    override fun onSurfaceChanged(p0: GL10?, w: Int, h: Int) {
+        GLES20.glViewport(0, 0, width, height)
     }
 
     override fun onDrawFrame(p0: GL10?) {
-        
+        GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
     }
 }
