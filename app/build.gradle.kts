c18b3de75ae671dd0629b55987c05e77ce803847
Alexoqool <154623493+Alexoqool@users.noreply.github.com>
2025-02-27 14:38:57 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "net.rpcs3"
-        minSdk = 29
+        minSdk = 31
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
